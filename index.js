d3b9bd6a65e17938aeb885709f6a6c8e1b8ab12b
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-08-05 13:06:27 -0400

    got initial server up and running. did some testing with Postman to verify it works. TWSS needs some training...
    Signed-off-by: Joshua Sandlin <joshua.sandlin@gmail.com>

--- index.js
+++ index.js
@@ -5,4 +5,50 @@ var server = restify.createServer({
     // config stuff
 });
 
-server.listen(8000);
+server.use(restify.bodyParser());
+
+/**
+ * Our predicate function, determines if given phrase
+ * is TWSS or not
+ *
+ * @param {function} parser This is our TWSS module
+ * @param {string} phrase This is the phrase to be tested
+ * @return {boolean} true or false on the given input phrase  
+ */
+function is_that_what_she_said_p(parser, phrase) {
+    if( typeof phrase === 'string' ) {
+        return parser.is(phrase);
+    } else {
+        return undefined;
+    }
+}
+
+function responder(req, res, next) {
+    var phrase = req.body.phrase;
+    // we are dependency injecting the twss module
+    // in case we want to replace it later
+    var response = is_that_what_she_said_p(twss, phrase);
+    console.log('-> parsing request for "%s"', phrase);
+    res.send({phrase: phrase, is_twss: response});
+    console.log('-> response sent');
+    return next();
+}
+
+/**
+ * Our main endpoint
+ *
+ * @params {string} our PATH
+ * @params {function} responder Our handler
+ * @return {string} a JSON object with the results of the query
+ */
+server.post('/api', responder);
+
+/**
+ * Setting our server up to listen
+ *
+ * @params {number} port Port that our server is running on
+ * @params {function} callback A function to run when the server starts
+ */
+server.listen(8000, function() {
+    console.log('**** %s listening on %s ****', server.name, server.url);
+});
