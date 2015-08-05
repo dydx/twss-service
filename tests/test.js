cf5f846892602846d94c2a50b8e8bda566dd7ecc
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-08-05 13:34:43 -0400

    taking a step back on the tests. need to implement some regression testing on the API

--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,1 @@
-var chai = require('chai'),
-    expect = chai.expect,
-    should = chai.should;
-
-var twss_function = require('../index.js');
-
-var parser = {
-    is: function(phrase) {
-        if (typeof phrase === 'string') {
-            return true;
-        } else {
-            return undefined;
-        }
-    }
-}
-
-var test1 = twss_function(parser, 'is that what she said');
-expect(test1).to.be.a('boolean').and.be.true;
+// still working on this stuff... :(
